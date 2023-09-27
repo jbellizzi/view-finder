40d76678d2dcce7e699f0351d78a722e4713c4a2
John Bellizzi <jabellizzi406@gmail.com>
2023-09-27 14:37:54 -0400

    pudate name and config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,20 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react/jsx-runtime",
+    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ["react-refresh", "prettier"],
   rules: {
     "@typescript-eslint/no-explicit-any": "warn",
